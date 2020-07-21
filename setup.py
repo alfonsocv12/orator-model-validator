6c03a42f9a1a4e49eb0fc73aa452be7bf9c30396
alfonsocv12 <alfonsocvu@gmail.com>
2020-07-21 12:39:29 -0600

    Fixin typo on classifier

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     packages=setuptools.find_packages(),
     classifiers=[
-        "Progamming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
