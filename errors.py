4b0b57a9cafb511119585b3c1b8935080323c34d
alfonsocv12 <alfonsocvu@gmail.com>
2021-02-17 16:28:25 -0700

    Adding new error file

---
+++ errors.py
@@ -0,0 +1,10 @@
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class ValidatorError(Error):
+    """Raised when the validator find and error"""
+
+    def __init__(self, status_code=None, body=None):
+        self.status_code = status_code
+        self.body = body
